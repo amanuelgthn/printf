88c7c43802691867f4fca2650354046c63c2e489
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-25 10:27:00 +0800

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -11,47 +11,42 @@
 int _printf(const char *format, ...)
 {
 	int i = 0, printed = 0, printed_chars = 0;
-	int flags, width, precision, size ,buff_ind = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
-	
+
 	if (format == NULL)
 	{
 		return (-1);
 	}
 	va_start(list, format);
-	
+
 	for (; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
-			{
 				print_buffer(buffer, &buff_ind);
-			}
 			printed_chars++;
 		}
 		else
 		{
-			print_buffer(buffer,&buff_ind);
+			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
+			printed = handle_print(format, &i, list, buffer,
+					       flags, width, precision, size);
 			if (printed == -1)
-			{
 				return (-1);
-			}
 			printed_chars += printed;
 		}
 	}
 	print_buffer(buffer, &buff_ind);
-	
 	va_end(list);
-	
 	return (printed_chars);
 }
 /**
