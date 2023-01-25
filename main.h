bf17123b7c0116910520906c57788968dfbb215c
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-25 09:18:47 +0800

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 int _printf(const char *format, ...);
+int _putchar(char c);
 void printf_buffer(char buffer[], int *buff_ind);
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
 #endif /* _MAIN_H_ */
