fbe8ae067761c0560f4365b014e29ef016d45c8f
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-25 10:32:58 +0800

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -49,16 +49,3 @@ int _printf(const char *format, ...)
 	va_end(list);
 	return (printed_chars);
 }
-/**
-*print_buffer- function to print the buffer if exists
-*@buffer: an array of chars of buffer
-*@buff_ind: number of char
-*/
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-	{
-		write(1, &buffer[0], *buff_ind);
-	}
-	*buff_ind = 0;
-}
