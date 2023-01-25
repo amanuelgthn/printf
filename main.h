0e97a78c55f56af6eec028cc8c383087abdc2359
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-25 09:50:28 +0800

    Update main.h

--- main.h
+++ main.h
@@ -3,6 +3,6 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 void printf_buffer(char buffer[], int *buff_ind);
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size);
 int get_flags(const char *format, int *i);
 #endif /* _MAIN_H_ */
