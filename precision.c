09a5f8fc75b6235fa5afb204762357dda20bf039
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-01-25 09:35:11 +0800

    Create precision.c

---
+++ precision.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include<stdlib.h>
+#include<stdlib.h>
+#include"main.h"
+/**
+*get_precision-precision for printing
+*@format: string to print arguments
+*@i: arguments to be printed
+*Return: precision
+**/
+int get_precision(const char *format, int *i, va_list list)
+{
+	int curr_i = *i + 1;
+	int precision = -1;
+
+	if (format[curr_i] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (precision);
+}
